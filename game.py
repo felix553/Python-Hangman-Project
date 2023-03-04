2057021fcce47999f22e341976949cd68f2a2263
felix553 <micxie@student.unimelb.edu.au>
2023-03-04 16:26:22 +1100

    Fixed display and added in hangman text art as well as play again function

--- game.py
+++ game.py
@@ -31,15 +31,79 @@ def initialise_game():
     global guesses
     global display
     global already_guessed
+    global game_over
+    game_over = 1
     word = choose_words()
     length = len(word)
-    strike = int(1 + length/2)
+    strike = 8
     guesses = 0
     display = '_' * length
     already_guessed = []
 
 def play_again():
-    return
+    start = input("Hit Enter to keep playing, or Hit 'q' to quit \n")
+    while(start):
+        if start == "e":
+            initialise_game()
+            hangman()
+            break
+        elif start == "q":
+            print("\nThe game will now end")
+            print("\nThanks for playing!")
+            break
+        else:
+            start = input("\nHit Enter to keep playing, or Hit 'q' to quit")
+
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
 
 def hangman():
     global word
@@ -48,41 +112,47 @@ def hangman():
     global guesses
     global display
     global already_guessed
+    global game_over
     print("\nThe Game Will Now Begin, Good Luck " + name + "!\n")
-    while (strike > 0):
-        print("Strikes: " + str(strike) + "   Guesses: " + str(guesses))
+    while (strike > 1):
+        print("Strikes: " + str(strike - 1) + "   Guesses: " + str(guesses))
         print ("Already Guessed Characters: " + str(already_guessed))
         print(display + "\n")
-        guess = input("Please enter your guess: ")
+        guess = input("Please enter your guess: ").upper()
         while (guess):
             if guess in already_guessed:
                 guess = input("\nYou already guessed " + guess + ", guess another character: ")
             elif guess in word:
                 print("\nYou guessed right!\n")
-
                 new_display = ""
-                for char in display:
+                for char, char2 in zip(word, display):
                     if char == guess:
-                        new_display+= char
-                    else:
+                        new_display += char
+                    elif char2 != '_':
+                        new_display += char2
+                    else: 
                         new_display+= '_'
                 display = new_display
-                print("display: " + display)
-                    
                 break
 
 
             else:
                 print("\nYou guessed wrong!\n")
-                strike += 1
+                strike -= 1
+                print(HANGMANPICS[-int(strike)] + '\n')
                 break
 
         already_guessed.append(guess)
         guesses += 1
         if word == display:
-            print("Congratulations! You have guessed the word: " + word + " in " + str(guesses) + "guesses\n")
+            print("Congratulations! You have guessed the word: " + word + " in " + str(guesses) + " guesses\n")
+            game_over = 0
             break
-    print("Game Over! You have run out of guesses!\n")
+    if game_over:
+        print("Game Over! You have run out of guesses!\n")
+    
+    play_again()
+
 
     return 
 
@@ -91,7 +161,7 @@ def choose_words():
     word = input("\nEnter word to guess: ")
     while(word): 
         if len(word) >= 3:
-            return word
+            return word.upper()
         else:
             word = input("\nPlease enter longer word: ")
 
